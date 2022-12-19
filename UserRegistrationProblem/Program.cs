9c3a851de311f88e26512e40c0bb78079f44220d
BashaVali <bashavali222@gmail.com>
2022-12-20 03:27:40 +0530

    User Registration Problem

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace UserRegistrationProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            UserRegistration user = new UserRegistration();
+            Console.WriteLine("Welcome to User Registration Problem Statements.");
+            bool choice = true;
+            while (choice)
+            {
+                Console.WriteLine("Enter your choice");
+                Console.WriteLine("1. Enter First Name\n2. Exit");
+                int condition = Convert.ToInt32(Console.ReadLine());
+                switch (condition)
+                {
+                    case 1:
+                        Console.WriteLine("Enter name - ");
+                        user.FirstName();
+                        break;
+                    default:
+                        Console.WriteLine("Oops,Try Again!");
+                        choice = false;
+                        break;
+                }
+            }
+        }
+    }
+}
